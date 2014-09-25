496725af26fd1f4fb7412a8e8eaacd8aa436f230
scizeron <stephane.cizeron@gmail.com>
2014-09-25 09:20:50 +0200

    auto-proxy a true cglib

--- rest-controller/src/main/java/test/AppConfig.java
+++ rest-controller/src/main/java/test/AppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-@EnableAspectJAutoProxy(proxyTargetClass=false)
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 public class AppConfig {
 
 }
