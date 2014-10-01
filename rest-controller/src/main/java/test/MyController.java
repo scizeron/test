3e62867981e11ae1e7ccb730b2aacf4f968f9631
scizeron <stephane.cizeron@gmail.com>
2014-10-01 15:48:28 +0200

    MAJ from upstream

--- rest-controller/src/main/java/test/MyController.java
+++ rest-controller/src/main/java/test/MyController.java
@@ -2,6 +2,7 @@ package test;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,11 @@ public class MyController implements MyInterface {
   @RequestMapping(value="/process", produces={MediaType.APPLICATION_JSON_VALUE})
   public String process() {
     return "ok";
+  }
+  
+  @RequestMapping(value="/required", produces={MediaType.APPLICATION_JSON_VALUE})
+  public ResultObject required(@RequestParam(value="type") String type) {
+    return new ResultObject("required", true);
   }
 
 }
